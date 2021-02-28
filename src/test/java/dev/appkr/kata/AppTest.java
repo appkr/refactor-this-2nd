8eb79e84cdd0359967ff22baabd6922d4bdd3da1
appkr <juwonkim@me.com>
2021-02-28 18:14:47 +0900

    created a project

---
+++ src/test/java/dev/appkr/kata/AppTest.java
@@ -0,0 +1,12 @@
+package dev.appkr.kata;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    public void contextLoads() {
+        assertTrue(true);
+    }
+}
