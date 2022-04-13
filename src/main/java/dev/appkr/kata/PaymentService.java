86ea6945b3971b48df1dddf374abd3d97492b81d
appkr <juwonkim@me.com>
2022-04-14 08:50:28 +0900

    Q3 추가

--- src/main/java/dev/appkr/kata/PaymentService.java
+++ src/main/java/dev/appkr/kata/PaymentService.java
@@ -2,13 +2,18 @@ package dev.appkr.kata;
 
 public class PaymentService {
 
+  // 동시성 문제는 고려하지 않는다
+  // 현재 코드에서 버그가 발생하는 지점
+  private long paypalBalance = 100_000L;
+
   public String payWithCreditcard(long amount) {
     // 신용카드사와 통신하여 결제 처리하는 복잡한 로직
-    return "신용카드로 " + amount + "원을 결제합니다";
+    return String.format("신용카드로 %d원을 결제합니다", amount);
   }
 
   public String payWithPaypal(long amount) {
     // Paypal과 통신하여 결제 처리하는 복잡한 로직
-    return "페이팔로 " + amount + "원을 결제합니다";
+    paypalBalance = paypalBalance - amount;
+    return String.format("페이팔로 %d원을 결제합니다, 잔액은 %d원입니다", amount, paypalBalance);
   }
 }
